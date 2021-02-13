5435dfed0230852f3d56fed4a0690d247b074668
MrStark75 <55640451+MrStark75@users.noreply.github.com>
2021-02-12 21:42:54 -0800

    Added if the command doesn't exist to return

--- events/guild/message.js
+++ events/guild/message.js
@@ -12,6 +12,7 @@ module.exports = (client, Discord, deletedMessages, editedMessages, message) => 
     const cmd = args.shift().toLowerCase();
 
     const command = client.commands.get(cmd) || client.commands.find(a => a.aliases && a.aliases.includes(cmd));
+    if (!command) return;
 
     if (!cooldowns.has(command.name)) {
         cooldowns.set(command.name, new Discord.Collection());
