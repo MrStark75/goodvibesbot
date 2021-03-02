8b0e4fdef0a88148eb4ed71f24a900fea4ce1338
MrStark75 <55640451+MrStark75@users.noreply.github.com>
2021-03-01 21:16:51 -0800

    Updated steal command to have 20% of failing

--- commands/steal.js
+++ commands/steal.js
@@ -10,7 +10,7 @@ module.exports = {
             return message.reply(`Please use a mention to specify the user you want to steal from.`)
         }
 
-        const caught = [true, false];
+        const caught = [true, false, false, false];
         const randomCaught = caught[Math.floor(Math.random() * caught.length)];
 
         const random = Math.floor(Math.random() * 80) + 1;
@@ -18,26 +18,32 @@ module.exports = {
         const targetMentions = (args[0]);
 
         if (target) {
-            const stealCoinsFromTarget = await profileModel.findOneAndUpdate({
-                userID: target
-            }, {
-                $inc: { coins: -random }
-            });
 
-            if (!stealCoinsFromTarget) {
-                return message.reply(`This user does not have an active profile with this bot.`);
+            if (randomCaught === true) {
+                message.reply(`You were caught stealing from ${targetMentions} and did not recieve any coins!`);
             }
 
-            const addCoinsToUser = await profileModel.findOneAndUpdate({
-                userID: message.author.id
-            }, {
-                $inc: { coins: random }
-            })
-
-            if (!addCoinsToUser) {
-                return message.reply('Please create an account by using `%beg`, then use steal.');
+            if (randomCaught === false) {
+                const stealCoinsFromTarget = await profileModel.findOneAndUpdate({
+                    userID: target
+                }, {
+                    $inc: { coins: -random }
+                });
+    
+                if (!stealCoinsFromTarget) {
+                    return message.reply(`This user does not have an active profile with this bot.`);
+                }
+    
+                const addCoinsToUser = await profileModel.findOneAndUpdate({
+                    userID: message.author.id
+                }, {
+                    $inc: { coins: random }
+                })
+    
+                if (!addCoinsToUser) {
+                    return message.reply('Please create an account by using `%beg`, then use steal.');
+                }
             }
-            
         }
 
         return message.channel.send(`You stole ${random} coins from ${targetMentions}`);
