c91611dd566f84a89f342c77c7ddba1d28dd2773
MrStark75 <55640451+MrStark75@users.noreply.github.com>
2021-03-19 18:33:03 -0700

    Added new vibe phrases

--- commands/vibe.js
+++ commands/vibe.js
@@ -8,7 +8,8 @@ module.exports = {
         "Lavender.", "Get glowing. The world needs your Magic.", "You have two choices: evolve or repeat.", "Three months from now, you will thank yourself.", "Make sure to get some water.",
         "The key to being happy is knowing you have the power to choose what to accept and what to let go.", "Walk away from anything that gives you bad vibes. There is no need to explain or make sense of it. It's your life. Do what makes you happy.",
         "You've always had the power my dear, you just had to learn it for yourself.", "There will be haters, doubters, non-believers. And then there will be you, proving them wrong.", 
-        "In the middle of difficulty lies opportunity.", "Free spot avalible, DM me to put in some good vibes (DM Mr. Stark75)", "Nutsacks (Adel requested)", ":eye:", "Your :cool:"];
+        "In the middle of difficulty lies opportunity.", "Free spot avalible, DM me to put in some good vibes (DM Mr. Stark75)", "Nutsacks (Adel requested)", ":eye:", "Your :cool:", "Life is too short to always be serious at all times.",
+        "Don't think too much, just do what makes you happy."];
 
         const reply = replies[Math.floor(Math.random() * replies.length)];
         message.channel.send(reply);
